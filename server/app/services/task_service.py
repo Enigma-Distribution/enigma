f349bc9ecc6016191c68d7904ccb351ca112d35c
tushargithub44 <tushar.bapecha@somaiya.edu>
2022-02-16 21:11:53 +0530

    updated task create api

--- server/app/services/task_service.py
+++ server/app/services/task_service.py
@@ -21,24 +21,24 @@ def alltasks(current_user):
 @user_required
 def newtask(current_user):
     try:
-        data = request.get_json()
+        data = request.form
         # user_id = current_user.user_id
-        task_name = data['task_name']
-        task_description = data['task_description']
-        task_zip_file_id = data['task_zip_file_id']
-        datasource_size = data['datasource_size']
+        task_name = data.get('task_name')
+        task_description = data.get('task_description')
+        task_zip_file_id = data.get('task_zip_file_id')
+        datasource_size = data.get('datasource_size')
         task_status = False
         task = task_service.create_task(current_user, task_name, task_description, task_zip_file_id, datasource_size, task_status)
-         
+        
         #Divide the Datasource file
-        # datasource_file = request.files["datasource_file"]
+        datasource_file = request.files["datasource"]
         #Create Steps -> Add to db
         #Add Steps to Queue -> RabbitMQ
         
         return jsonify({"STATUS": "OK", "TASK": task})
     except Exception as e:
-        return jsonify({"alert": "Error!"})
-    return jsonify({"STATUS": "FAIL", "MSG": SERVER_ERROR}), 501
+        # return jsonify({"alert": "Error!"})
+        return jsonify({"STATUS": "FAIL", "MSG": SERVER_ERROR}), 501
 
 @app.route('/task', methods=['POST'])
 @user_required
