f304fcf9c2a318c84b41a50ba6c15fe4a8ddb4d0
tushargithub44 <tushar.bapecha@somaiya.edu>
2022-02-16 21:56:55 +0530

    updated upload file api

--- server/app/uploads/routes.py
+++ server/app/uploads/routes.py
@@ -11,19 +11,24 @@ uploads = Blueprint('uploads', __name__)
 
 @uploads.route("/upload-file", methods=['POST'])
 def upload_file():
+    print("Upload Filess!!")
     try:
         file = request.files['file']
+        task_file = request.form.get("task_file")
         if file:
             file_id = uuid.uuid4()
+            if task_file:
+                file_id = "Task-" + str(file_id)
+            filename = secure_filename(file_id)
             s3_bucket_instance = get_s3_bucket()
             s3_bucket_instance.put_object(
-                Key=file_id,
                 Body=file,
+                Key=file_id,
                 ACL='public-read'
             )
         return jsonify({"STATUS": "OK", "file_id": file_id})
     except Exception as e:
-        return jsonify({"STATUS": "FAIL", "MSG": SERVER_ERROR}), 501
+        return jsonify({"STATUS": "FAIL", "MSG": str(e)}), 501
 
 # s3.meta.client.download_file('BUCKET_NAME', 'FILENAME.txt', '/TASKID/FILENAME.txt')
 
