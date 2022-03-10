a38c2858282dc7ffbfc51297e6db493a0e71ab8b
Nimish Vithalani <nimish.v@somaiya.edu>
2022-03-11 00:18:02 +0530

    register component complete

---
+++ volunteers/src/renderer/pages/register.vue
@@ -0,0 +1,60 @@
+<template>
+  <section style="{ 'padding' : 5pxs }">
+      <div class="field">
+        <label class="label">Name</label>
+        <div class="control">
+            <input class="input" type="text" placeholder="Name" v-model="Name">
+        </div>
+        </div>
+      <div class="field">
+        <label class="label">Email</label>
+        <div class="control">
+            <input class="input" type="email" placeholder="Emails" v-model="Email">
+        </div>
+        </div>
+
+        <div class="field">
+        <label class="label">Password</label>
+        <div class="control">
+            <input class="input" type="password" placeholder="Password" v-model="Password">
+        </div>
+        </div>
+        <div class="field">
+        <div class="control">
+            <button class="button is-success" v-on:click="authenticate">Sign Up</button>
+        </div>
+        </div>
+        <div class="field">
+        <div class="control">
+            <nuxt-link class="button" :to="'/login'">Login</nuxt-link>
+        </div>
+        </div>
+
+  </section>
+</template>
+<script>
+import register from '../functions/register'
+export default {
+    data() {
+        return {
+            Name : "",
+            Email : "",
+            Password : ""
+        }
+
+    },
+
+    methods : {
+        async authenticate() {
+            const [status, data ] = await register(this.Email, this.Password, this.Name, this)
+            if(status){
+                this.$router.push('/')
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
