b8b7e44af5e89f0f5a2a42a7bf76f21aab9d9c7b
Nimish Vithalani <nimish.v@somaiya.edu>
2022-03-14 19:57:17 +0530

    logout button added

--- volunteers/src/renderer/layouts/default.vue
+++ volunteers/src/renderer/layouts/default.vue
@@ -1,48 +1,63 @@
 <template>
   <div>
     <b-navbar>
-        <template #brand>
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-                <img
-                    src="https://user-images.githubusercontent.com/39585600/147443810-7a847423-7980-4d80-9779-ce23c62eeb6d.png"
-                    alt="Enigma Logo"
-                >
-            </b-navbar-item>
-        </template>
-        <template #start>
-            <b-navbar-item tag="router-link" :to="{ path: '/tasks/all' }">
-                📥&nbsp;&nbsp;Computations
-            </b-navbar-item>
-            <b-navbar-item tag="router-link" :to="{ path: '/transactions' }">
-                💰&nbsp;&nbsp;Transactions
-            </b-navbar-item>
-            <b-navbar-item tag="router-link" :to="{ path: '/settings' }">
-                ⚙️&nbsp;&nbsp;Settings
-            </b-navbar-item>
-        </template>
+      <template #brand>
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img
+            src="https://user-images.githubusercontent.com/39585600/147443810-7a847423-7980-4d80-9779-ce23c62eeb6d.png"
+            alt="Enigma Logo"
+          />
+        </b-navbar-item>
+      </template>
+      <template #start>
+        <b-navbar-item tag="router-link" :to="{ path: '/tasks/all' }">
+          📥&nbsp;&nbsp;Computations
+        </b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/transactions' }">
+          💰&nbsp;&nbsp;Transactions
+        </b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/settings' }">
+          ⚙️&nbsp;&nbsp;Settings
+        </b-navbar-item>
+      </template>
 
-        <template #end>
-            <b-navbar-item tag="div">
-                <div class="buttons">
-                    <nuxt-link :to="'/register'" class="button is-primary">
-                        <strong>Sign up</strong>
-                    </nuxt-link>
-                    <nuxt-link :to="'/login'" class="button is-light">
-                        Log in
-                    </nuxt-link>
-                </div>
-            </b-navbar-item>
-        </template>
+      <template #end>
+        <b-navbar-item tag="div">
+          <div class="buttons" v-if="!isloggedin">
+            <nuxt-link :to="'/register'" class="button is-primary">
+              <strong>Sign up</strong>
+            </nuxt-link>
+            <nuxt-link :to="'/login'" class="button is-light">
+              Log in
+            </nuxt-link>
+          </div>
+          <div class="buttons" v-else>
+            <button v-on:click.prevent="logout" class="button is-light">
+              Logout
+            </button>
+          </div>
+        </b-navbar-item>
+      </template>
     </b-navbar>
-    <nuxt/>
+    <nuxt />
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'DefaultLayout',
-}
+  name: "DefaultLayout",
+  computed: {
+    isLoggedIn() {
+      return this.$store.state.isLoggedIn;
+    },
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch("logout");
+      this.$router.push("/login");
+    },
+  },
+};
 </script>
 
 <style>
