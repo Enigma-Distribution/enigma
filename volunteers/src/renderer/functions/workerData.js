f499993f4903763839b4141f1b9a043b14d3cf85
Nimish Vithalani <nimish.v@somaiya.edu>
2022-03-17 16:35:59 +0530

    api connection ready for fetching worker data

---
+++ volunteers/src/renderer/functions/workerData.js
@@ -0,0 +1,21 @@
+import baseURL from "./baseURL";
+import axios from "axios";
+
+export default async function (context) {
+    //context.$axios.setHeader('Content-Type', 'application/json')
+    const response = await axios.post(
+        `${baseURL}/worker/allot-me`,
+        {},
+        {
+            headers: {'Content-Type' : 'application/json'}
+        }
+    );
+
+    if (response.STATUS == "OK") {
+        return [response.STEP, undefined]
+    }
+    else if (response.STATUS == "FAIL") {
+        return [false, response.MSG]
+    }
+    return [false, "Problem with API call. Contact Developers."]
+}
