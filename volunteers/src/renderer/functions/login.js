bd7fea7815ddab0d452960a24590ed146cd09237
Nimish Vithalani <nimish.v@somaiya.edu>
2022-03-24 15:41:25 +0530

    login bug solved

--- volunteers/src/renderer/functions/login.js
+++ volunteers/src/renderer/functions/login.js
@@ -1,24 +1,26 @@
 import baseURL from "./baseURL.js";
 import axios from "axios";
 
-export default async function(email, password, context) {
+export default async function(email, password, user_type, context) {
     //return [true, undefined]
     //context.$axios.setHeader('Content-Type', 'application/json')
     const response = await axios.post(
         `${baseURL}/authenticate/user`,
         {
             email: email,
-            password: password
+            password: password,
+            user_type: user_type
         },
         {
             headers: {'Content-Type' : 'application/json'}
         }
     );
-    if (response.STATUS == "OK") {
-        context.$store.dispatch('setToken', response.TOKEN)
+    console.log(response)
+    if (response.data.STATUS == "OK") {
+        context.$store.dispatch('setToken', response.data.TOKEN)
         return [true, undefined]
     }
-    else if (response.STATUS == "FAIL") {
+    else if (response.data.STATUS == "FAIL") {
         return [false, response.MSG]
     }
     return [false, "Problem with API call. Contact Developers."]
--- volunteers/src/renderer/pages/login.vue
+++ volunteers/src/renderer/pages/login.vue
@@ -13,6 +13,14 @@
             <input class="input" type="password" placeholder="Password" v-model="Password">
         </div>
         </div>
+
+        <div class="field">
+        <label class="label">User Type</label>
+        <div class="control">
+            <input class="input" type="text" placeholder="Type" v-model="UserType">
+        </div>
+        </div>
+
         <div class="field">
         <div class="control">
             <button class="button is-success" v-on:click="authenticate">Login</button>
@@ -34,12 +42,13 @@ export default {
      return {
         Email : "",
         Password : "",
+        UserType: ""
      }
     },
 
     methods: {
         async authenticate(){
-           const [status, data] =  await login(this.Email, this.Password, this);
+           const [status, data] =  await login(this.Email, this.Password, this.UserType,this);
            if(status){
                this.$router.push('/')
            }
