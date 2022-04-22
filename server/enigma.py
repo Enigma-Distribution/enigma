c37ba35a774b437b31885cfd32a27742287f4afa
tushargithub44 <tushar.bapecha@somaiya.edu>
2022-04-22 12:06:53 +0530

    making deployement ready

--- server/enigma.py
+++ server/enigma.py
@@ -1,1 +1,28 @@
-from app import app
+# from app import app
+from app.properties import get_site_secret_key
+import os
+from flask import Flask
+from flask_cors import CORS
+app = Flask(__name__)  # , template_folder=template_dir
+app.config['SECRET_KEY'] = get_site_secret_key()
+
+CORS(app)
+
+# from app import services
+
+from app.services.auth_service import app as auth_service
+app.register_blueprint(auth_service)
+
+from app.services.task_service import app as task_service
+app.register_blueprint(task_service)
+
+from app.services.step_service import app as step_service
+app.register_blueprint(step_service)
+
+from app.services.worker_service import app as worker_service
+app.register_blueprint(worker_service)
+ 
+from app.uploads.routes import uploads
+app.register_blueprint(uploads)
+
+# app.run()
