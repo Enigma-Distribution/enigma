31ad09b992fadba76b746f0dae4840b3301e8c5d
Nimish Vithalani <nimish.v@somaiya.edu>
2022-05-04 11:25:31 +0530

    running each phase file in container

--- runtimes/python/main.py
+++ runtimes/python/main.py
@@ -1,38 +1,18 @@
 import os
-from asyncore import file_dispatcher
 import sys
-from distutils.command.upload import upload
-# from modules.unzipper import unzip
-# from modules.utils import run_if_setup_file_exists
-import ipfsUpload
-import requests
 
 from client.handler import app
 
-hash = ''
-
-url = sys.argv[1]
-os.environ["ENIGMA_FAE"] = url
+os.environ["ENIGMA_FAE"] =  sys.argv[1]
+os.environ["PHASE"] = sys.argv[2]
+os.environ["STEP_ID"] = sys.argv[3]
+os.environ["API_URL"] = "{}/worker/submit-result".format(sys.argv[4])
 
 if sys.argv[2] == "map":
-    map_data = app.run_mapper()
-    print('map_data', map_data)
-    hash = ipfsUpload(map_data)
-
+    app.run_mapper()
 
 if sys.argv[2] == "reduce":
-    reduce_data = app.run_reducer()
-    print('reduce_data', reduce_data)
-    hash = ipfsUpload(reduce_data)
-
+    app.run_reducer()
 
 if sys.argv[2] == "shuffle":
-    shuffle_data = app.run_shuffler()
-    print('shuffle_data', shuffle_data)
-    hash = ipfsUpload(shuffle_data)
-
-phase = sys.argv[2]
-step_id = sys.argv[3]
-url = "{}/worker/submit-result".format(sys.argv[4])
-params = {"phase" : phase, "step_id": step_id, "result_file_id": hash}
-r = requests.post(url, params=params)
+    app.run_shuffler()
