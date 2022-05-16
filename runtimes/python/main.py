8999f46da52b3506c9635488010d404e2124a430
Nimish Vithalani <nimish.v@somaiya.edu>
2022-05-17 01:54:10 +0530

    setting variables in env

--- runtimes/python/main.py
+++ runtimes/python/main.py
@@ -9,32 +9,6 @@ os.environ["STEP_ID"] = sys.argv[3]
 os.environ["API_URL"] = sys.argv[4]
 os.environ["AUTH_TOKEN"] = sys.argv[5]
 
-# def json_data():
-#     with open('data.json') as json_file:
-#         data = json.load(json_file)
-#         return data
-        
-
-# def append_and_write(taskName, taskData):
-#     data = json_data(filename='data.json')
-#     data[taskName] = taskData 
-#     json_object = json.dumps(data, indent = 1)
-#     with open("data.json", "w") as outfile:
-#         outfile.write(json_object)
-
-# def append_and_write(taskName, taskData):
-#     data = json_data(filename='data.json')
-#     data[taskName] = taskData 
-#     json_object = json.dumps(data, indent = 1)
-#     with open("data.json", "w") as outfile:
-#         outfile.write(json_object)
-
-# append_and_write("ENIGMA_FAE", sys.argv[1])
-# append_and_write("PHASE", sys.argv[2])
-# append_and_write("STEP_ID", sys.argv[3])
-# append_and_write("API_URL", sys.argv[4])
-# append_and_write("TOKEN", sys.argv[5])
-
 from client.handler import app
 
 if os.getenv("PHASE") == "map":
